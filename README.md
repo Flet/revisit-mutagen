883fcccaa18115c531016bdabe32d3cae0f25ba4
Dan Flettre <fletd01@yahoo.com>
2014-08-28 20:06:53 -0500

    update README and remove crazy comments

--- README.md
+++ README.md
@@ -2,3 +2,73 @@ revisit-mutagen
 ===============
 
 A hapi plugin to easily expose a http://revisit.link mutation as a service that adheres to http://revisit.link/spec.html
+
+This can be used to quickly spin up a server to serve
+
+
+the plugin options is expected to be an object with the key representing the base
+path and the value being the "mutator" function/module. Pass in one
+or many of these! Some example mutator npm modules: butts-gm, trippyshift
+
+```
+var options = {
+    "trippyshift": require('trippyshift'),
+    "butts": require('butts-gm'),
+    "mylittlemutant": function(buffer, callback) {
+        //awesome goes here
+        callback(null, someMutatedbuffer)
+    }
+};
+```
+
+The mutator should be a function that takes a buffer and a callback. It
+should then execute the callback with a buffer as the second argument
+or an error as the first argument.
+
+
+Full example Server:
+
+```
+var Hapi = require('hapi');
+
+var server = Hapi.createServer('localhost', process.env.PORT || 8080);
+
+server.pack.register({
+    plugin: require('revist-mutagen'),
+
+    // the "key" is the route to expose and the "value" is a mutator function/module
+    ```
+    options: {
+        'trippyshift': require('trippyshift'),
+        'butts': require('butts-gm'),
+        'echoplease': function(buffer, callback) {
+            // you can just write your own "mutator" inline too!
+            callback(null, buffer);
+        }
+    }
+    ```
+
+}, function (err) {
+    if (err) throw err;
+    server.start(function () {
+
+        // list out all the routes for verification
+        server.table().forEach(function (row) {
+            console.log(server.info.uri + row.path + " (" + row.method + ")");
+        });
+
+        console.log("Hapi server started @", server.info.uri);
+    });
+
+});
+```
+Start the server up (`node myfile.js`) and check the output:
+```
+http://localhost:8080/butts/ (head)
+http://localhost:8080/echoplease/ (head)
+http://localhost:8080/trippyshift/ (head)
+http://localhost:8080/butts/service (post)
+http://localhost:8080/echoplease/service (post)
+http://localhost:8080/trippyshift/service (post)
+Hapi server started @ http://localhost:8080
+```
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Joi = require('joi'),
     dataUriToBuffer = require('data-uri-to-buffer');
 
+// http://revisit.link/spec.html
 var revisitorSchema = {
     content: Joi.string().max(2000000),
     meta: Joi.object().required()
@@ -9,64 +10,24 @@ var revisitorSchema = {
 exports.register = function (plugin, options, next) {
 
     if (Array.isArray(options)) {
-        /* TODO: support an array of "long form" objects for convenience and
-        future potential to pass mutator-specific options object.
-
-        example:
-
-        var foos = [{
-            name: "trippyshift",
-            mutator: require('trippyshift'),
-            options: {
-                shiftXMaxPercent: 0.2
-            }
-        }, {
-            name: "butts",
-            mutator: require('butts-gm')
-        }];
-
-        The mutator options object could be passed as the first argument to the
-        mutator instead of the buffer:
-            ...
-            mutator(mutatorOptions, imgBuf, function (err, mutatedBuffer) {
-            ...
-        */
-        throw new Error('Arrays are not yet supported... Patches welcome?');
+        //TODO: support an array of "long form" objects and future potential to pass a mutator-specific options object.
+        throw new Error('Arrays of endpoints are not yet supported... Patches welcome?');
     } else {
-
-        /*
-        options is expected to be an object with the key representing the base
-        path and the value being the mutator function/module.require. Pass in one
-        or many of these!
-
-            var options = {
-                "trippyshift": require('trippyshift'),
-                "butts": require('butts-gm'),
-                "mylittlemutant": function(buffer, callback) {
-                    //awesome goes here
-                    callback(null, someMutatedbuffer)
-                }
-            };
-
-        The mutator should be a function that takes a buffer and a callback. It
-        should then execute the callback with a buffer as the second argument
-        or an error as the first argument.
-
-        */
 
         Object.keys(options).forEach(function (key) {
             buildRevisitorRoutes(key, options[key]);
         });
     }
 
-
     function buildRevisitorRoutes(basePath, mutator) {
-        // http://revisit.link/spec.html
 
         if (!basePath) throw new Error('No name specified!. Please give me a name!');
         if (!mutator) throw new Error('No mutator function passed for ' + basePath + '! Please pass in a mutator!');
 
-        //  HEAD / where it can return a 200 response when we ping the server to see if it is up.
+        //TODO: add support mutator to be an array of mutators. Just send the content through each one in order every time.
+
+
+        // HEAD - return a 200 response when the hub pings the server to see if it is up.
         plugin.route({
             method: 'HEAD',
             path: '/' + basePath + '/',
@@ -75,6 +36,7 @@ exports.register = function (plugin, options, next) {
             }
         });
 
+        // POST - run the content through the mutator and return a revisit.link compatible object
         plugin.route({
             method: 'POST',
             path: '/' + basePath + "/service",
